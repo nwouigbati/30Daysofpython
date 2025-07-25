b456ad62952c8003ddc44af3c6ac4c606f103887
root <root@PATRICEMILLIARDS85.localdomain>
2025-07-25 20:44:35 +0000

    first commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,27 @@
+import math
+# exercice niveau 1
+# n°1, vérification de la version python utilisé
+# syntaxe : python --version
+
+# 2 opération arithmétique
+print(3+4)
+print(3-4)
+print(3*4)
+print(4%3)
+print(4//3)
+print(4**3)
+
+# 3 chaines de caractères
+print("O votre nom o votre prénom")
+print("o votre pays o je profite de 30 jours")
+print("de python")
+
+# 4 vérification des types de données
+
+print(type(3.14))
+
+#exercice niveau 2
+# distance euclidienne entre (2,3) et (10,8)
+
+d = math.sqrt((10-2)^2 + (8-3)^2)
+print(d)
---
+++ helloword.py
@@ -0,0 +1,7 @@
+print(2+3) #addition
+print(3-1) #soustraction
+print(2*3) #multipplication
+print(3/2) #division
+print(3**2) #exposant
+print(3%2) #modulo
+print(3//2) #division entière
