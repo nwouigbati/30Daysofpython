7925c975662f578d0cc300af2996b285adbfb9b4
root <root@PATRICEMILLIARDS85.localdomain>
2025-07-25 23:29:45 +0000

    mise à jour

---
+++ day_2/variables.py
@@ -0,0 +1,20 @@
+# <<jour 2: 30 Days of python programming>>
+
+#exercice 1
+
+firstName = "Gbati Patrice"
+lastName = "N'WOUI"
+fullName = "N'WOUI Gbati Patrice"
+country = "Togo"
+municipal = "maritime"
+year = 2025
+is_married = "no"
+is_true = "yes"
+is_light_on = "true"
+age,sex = 22, "M"
+
+print("my name is ", fullName, "I am ", age, "years old and I come from ", country)
+# exercice 2
+# le reste de l'exercice peut être fait 
+
+
